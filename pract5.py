bc35132be5cba86f6f205114632c22a9a1cd004d
KIRILL-SMOLIN <kirill-smol@mail.ru>
2024-05-20 00:23:38 +0500

    Add files via upload

---
+++ pract5.py
@@ -0,0 +1,6 @@
+def print_params(param):
+    print(param*2)  # Это если дословно читать условие задания
+    print(param)
+    print(param)  # Это если подсмотреть советы в чате)
+
+print_params('я 2 раза не повторяю ')
